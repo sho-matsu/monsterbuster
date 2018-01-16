e941f1b14a3852d678e21f9dc86147e27733a047
sho <born_in_april_89@yahoo.co.jp>
2018-01-16 18:26:29 +0900

    first commit

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("JoinRandom");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+}
