5d7bc0a3d4bfb47619429f4c282e3719f93079cf
sho <born_in_april_89@yahoo.co.jp>
2018-02-02 17:10:29 +0900

    GameObject release

--- Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/NetworkMonster.cs
+++ Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/NetworkMonster.cs
@@ -100,8 +100,6 @@ public class NetworkMonster : Photon.PunBehaviour, IPointerDownHandler, IPointer
     [PunRPC]
     public void Attack()
     {
-        if (!enableAttack) return;
-
         // 攻撃アニメーションを取得し、GameObjectに設定
         // 攻撃し、パーティクルでエフェクトを表示したあと、GameObjectを破棄
         StartCoroutine(RunAttack());
@@ -109,39 +107,41 @@ public class NetworkMonster : Photon.PunBehaviour, IPointerDownHandler, IPointer
 
     private IEnumerator RunAttack()
     {
-        var monster = photonView.gameObject;
-        var rb = monster.GetComponent<Rigidbody>();
-        // モンスターにかかっている物理力をリセット
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-        rb.ResetInertiaTensor();
-
-        // 攻撃アニメーション
-        var anim = monster.GetComponent<Animator>();
-        anim.Play("Attack");
-        // ステートの反映に1フレームいる
-        yield return null;
-
-        if (photonView.isMine)
+        if (enableAttack)
         {
-            // 攻撃時のエフェクト
-            GameObject fireBall = PhotonNetwork.Instantiate("FireBall", transform.position + new Vector3(0.0f, 2.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation, 0);
-            fireBall.transform.LookAt(target.transform);
-            var ballRb = fireBall.GetComponent<Rigidbody>();
-            ballRb.velocity = fireBall.transform.forward * 15;
+            var monster = photonView.gameObject;
+            var rb = monster.GetComponent<Rigidbody>();
+            // モンスターにかかっている物理力をリセット
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.ResetInertiaTensor();
+
+            // 攻撃アニメーション
+            var anim = monster.GetComponent<Animator>();
+            anim.Play("Attack");
+            // ステートの反映に1フレームいる
+            yield return null;
+
+            if (photonView.isMine)
+            {
+                // 攻撃時のエフェクト
+                GameObject fireBall = PhotonNetwork.Instantiate("FireBall", transform.position + new Vector3(0.0f, 2.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation, 0);
+                fireBall.transform.LookAt(target.transform);
+                var ballRb = fireBall.GetComponent<Rigidbody>();
+                ballRb.velocity = fireBall.transform.forward * 15;
+            }
+
+            //var targetPos = target.transform.position;
+            //var pos = new Vector3(targetPos.x, 1.5f, targetPos.z - 1);
+            //GameObject fire = PhotonNetwork.Instantiate("Fire", pos, Quaternion.identity, 0);
+            //fire.SetActive(true);
+            //// プレイヤーにダメージ
+            //var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PhotonView>();
+            //player.RPC("ReceiveDamage", PhotonTargets.All);
+
+            // アニメーションが終わるまで待つ
+            yield return new WaitWhile(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1);
         }
-
-        //var targetPos = target.transform.position;
-        //var pos = new Vector3(targetPos.x, 1.5f, targetPos.z - 1);
-        //GameObject fire = PhotonNetwork.Instantiate("Fire", pos, Quaternion.identity, 0);
-        //fire.SetActive(true);
-        //// プレイヤーにダメージ
-        //var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PhotonView>();
-        //player.RPC("ReceiveDamage", PhotonTargets.All);
-
-        // アニメーションが終わるまで待つ
-        yield return new WaitWhile(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1);
-
         // モンスター破棄
         if (photonView.isMine)
         {
