6388fc59a2d58600591d52751ee7c504c28190f7
sho <born_in_april_89@yahoo.co.jp>
2018-02-13 17:19:41 +0900

    add vive plugin

---
+++ Assets/MonsterBuster/Manager/Scripts/Controller/UserActionAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserActionAttack : MonoBehaviour {
+    SteamVR_TrackedObject trackedObject;
+    SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            // todo モンスターへの攻撃
+            device.TriggerHapticPulse(1000);
+        }
+	}
+}
